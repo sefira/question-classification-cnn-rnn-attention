fca10c97019f81f9694427ac6d4f647860a77c77
Xingyuan Bu <sefira32@gmail.com>
2017-06-19 06:27:37 +0000

    ready to trans to indiejoseph text_cnn.py

--- src/text_cnn.py
+++ src/text_cnn.py
@@ -1,6 +1,4 @@
 import tensorflow as tf
-import numpy as np
-
 
 class TextCNN(object):
     """
@@ -21,10 +19,10 @@ class TextCNN(object):
 
         # Embedding layer
         with tf.device('/cpu:0'), tf.name_scope("embedding"):
-            self.W = tf.Variable(
+            W = tf.Variable(
                 tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
                 name="W")
-            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
+            self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
             self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
 
         # Create a convolution + maxpool layer for each filter size
